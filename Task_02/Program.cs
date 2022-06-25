714556c5bade1a250a545c62fcf256ffb03d851c
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-25 22:21:38 +0300

    task 02. add help command, user data structure

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,10 +1,28 @@
 ﻿/*
 * Задача 2
-  todo: Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+  v 0.0 Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+  todo: v 0.1 Продолжаем прокачивать приложение с командами. Добавить к программе, которая заканчивается, 
+    когда пишем exit еще 4 команды (их можно придумать самому). 
+    Например: 
+        SetName – Установить имя 
+        Help – вывести список команд 
+        SetPassword – Установить пароль 
+        Exit – выход 
+        WriteName – вывести имя после ввода пароля
 */
 
-// "кодовое" слово для завершения программы
+
+
+// возможные команды
 const string exitWord = "exit";
+
+// индексы в пользовательской информации 
+const int userName = 0;
+const int userPass = 1;
+
+// пользовательская информация
+string[] userInfo = { string.Empty, string.Empty };
 
 // пользовательский ввод 
 // параметры:
@@ -17,23 +35,90 @@ string InputCommand(string message)
     return Console.ReadLine();
 }
 
-// регистронезависимое сравнение пользовательской команды с кодовым словом завершения
+// сообщениеи для проьзователя
+// параметрыЖ
+//      message - выводимое сообщение
+void ShowMsg(string message)
+{
+    Console.WriteLine(message);
+    Console.WriteLine("Press any key to continue ...");
+    Console.ReadKey();
+}
+
+// вывод справки
+// параметр:
+//      нет
+//возврат:
+//      нет
+void PrintHelp()
+{
+    Console.WriteLine("System help ");
+    Console.WriteLine("\thelp                  - print program help");
+    Console.WriteLine("\tset name [<username>] - set default user name");
+    Console.WriteLine("\t\t   <username> - user name. If name is empty, program will ask it");
+    Console.WriteLine("\tset password          - set system password");
+    Console.WriteLine("\tget name              - print default user name after authorisation");
+    Console.WriteLine("\texit                  - close program");
+}
+
+// обработчик команды пользователя, парсит пользовательский ввод. в зависимости от введенной команды 
+// вызывает соответсвтвующий обработчик, возвращает признак завершения приложения
 // параметры:
 //      userWord - введенная пользователем команда
 // возврат:
-//      true - если userWord совпадает с командой выхода
-//      false - если userWord не совпадает с командой выхода
-bool CommandExit(string userWord)
+//      true - завершение программы
+//      false - продолжение работы
+bool CommandProcessor(string userWord)
 {
-    bool result = false;
+    bool canExit = false;
+    string[] command = userWord.Split(' '); 
 
-    if (userWord.ToLower() == exitWord.ToLower())
+    if (command.Length == 0)
     {
-        result = true;
+        ShowMsg("Wrong command!\nUse help command for information.");
+        return canExit;
     }
 
-    return result;
+    // команду в нижний регистр, избавляемся от лишних ToLover в проверках
+    command[0] = command[0].ToLower();
+    if(command.Length > 1)
+        command[1] = command[1].ToLower();
+
+    
+    if(command[0] == "help"){
+        PrintHelp();
+    }
+    else if(command[0] == "exit")
+    {
+        canExit = true;
+    }
+    else if(command.Length <2 ){
+        ShowMsg("Wrong command format!\nUse help command for information.");
+    }
+    else if(command[0] == "get" && command[1] == "name"){
+        // todo вызов отображения именни пользователя
+    }
+    else if(command[0] == "set" && command[1] == "name"){
+        if(command.Length > 2)
+        {
+            // todo вызов ввода имевни с заданным параметром
+        }
+        else
+        {
+            // todo вызов задания имени с запросом параметра
+        }
+    }
+    else if(command[0] == "set" && command[1] == "password"){
+        // todo запрос пароля
+    }
+    else{
+        ShowMsg("Wrong command format!\nUse help command for information.");
+    }
+
+    return canExit;
 }
+
+
 
 // main body
 string command;         // пользовательский ввод
@@ -42,8 +127,10 @@ Console.Clear();
 
 while (true)            // работаем бесконечно
 {
-    command = InputCommand("Input command: ");
-    if (CommandExit(command))
+    //PrintHelp();
+
+    command = InputCommand("> ");
+    if (CommandProcessor(command))
     {
         break;
     }
